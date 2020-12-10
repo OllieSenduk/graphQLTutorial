69e740a748ad2ea30556f7e9b461b41db7d70a03
Oliver Senduk <oliversenduk@olivers-mbp.home>
2020-12-10 20:15:16 +0100

    created apollo server

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const { apolloServer, ApolloServer } = require('apollo-server')
+
+// Schema
+// The typeDefs constant defines your GraphQL
+// schema (more about this in a bit). Here, it 
+// defines a simple Query type with one field 
+// called info. This field has the type String!. 
+// The exclamation mark in the type definition 
+// means that this field is required 
+// and can never be null.
+const typeDefs = `
+    type Query {
+        info: String!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        info: () => 'This is the API of a Hackernews clone'
+    }
+}
+
+// Server
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server
+.lister()
+.then(({ url }) =>
+console.log(`Server is running on ${url}`)
+)
