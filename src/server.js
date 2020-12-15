5dd03284dd40b6300973b4aed71f1b6f2dacbb91
Oliver Senduk <oliversenduk@olivers-mbp.home>
2020-12-15 17:35:45 +0100

    Add resolvers

--- src/server.js
+++ src/server.js
@@ -9,17 +9,37 @@ const { apolloServer, ApolloServer } = require('apollo-server')
 // means that this field is required 
 // and can never be null.
 const typeDefs = `
-    type Query {
-        info: String!
-    }
+  type Query {
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
 `
 
-// Resolvers
-const resolvers = {
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+  
+  const resolvers = {
     Query: {
-        info: () => "Not null"
+      info: () => `This is the API of a Hackernews Clone`,
+      // 2
+      feed: () => links,
+    },
+    // 3
+    Link: {
+      id: (parent) => parent.id,
+      description: (parent) => parent.description,
+      url: (parent) => parent.url,
     }
-}
+  }
 
 // Server
 const server = new ApolloServer({
