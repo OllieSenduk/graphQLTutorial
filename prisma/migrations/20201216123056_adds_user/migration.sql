358cf2aced314c6773931375b3603bc1831f193c
Oliver Senduk <ollie.senduk@gmail.com>
2020-12-16 13:31:10 +0100

    Adds prisma migration

---
+++ prisma/migrations/20201216123056_adds_user/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE "Link" ADD COLUMN     "postedById" INTEGER;
+
+-- CreateTable
+CREATE TABLE "User" (
+"id" SERIAL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Link" ADD FOREIGN KEY("postedById")REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
