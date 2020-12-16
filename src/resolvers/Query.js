a5834df1832b58a15b3e5e71acacff483eae332f
Oliver Senduk <ollie.senduk@gmail.com>
2020-12-16 15:18:54 +0100

    Adds new resolvers folder

---
+++ src/resolvers/Query.js
@@ -0,0 +1,12 @@
+function feed(parent, args, context, info) {
+    return context.prisma.link.findMany()
+}
+
+function info(parent, args, context, info) {
+    return 'GraphQL query'
+}
+
+module.exports = {
+    feed,
+    info
+}
